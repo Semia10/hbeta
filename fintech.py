140c056a2cb5427cb072a205a9c59fd075194ce9
mingyaang <mmao@ualberta.ca>
2018-11-17 14:56:17 -0700

    oijoijrq

--- fintech.py
+++ fintech.py
@@ -6,13 +6,6 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.layers import Dense
 
-def build_model(input_size, output_size):
-    model = Sequential()
-    model.add(Dense(256, input_shape=(input_size,), activation='softmax'))
-    model.add(Dense(units=output_size , activation='softmax'))
-    model.compile(optimizer=Adam(), loss='mse')
-
-    return model
 
 
 
